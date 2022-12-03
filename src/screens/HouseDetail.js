9421afb07d3ff3c1c71050920a75b0c743e2b12b
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2022-12-03 20:58:40 +0100

    final design

--- src/screens/HouseDetail.js
+++ src/screens/HouseDetail.js
@@ -6,42 +6,43 @@ import StraightenOutlinedIcon from '@mui/icons-material/StraightenOutlined';
 function HouseDetail({data}) {
     const {id} = useParams();
     const houseData = data[id];
+    const colorMain = '#56DDC5';
 
     return (
       <div className="house-detail">
         <div className="card">
           <div className="main-content">
-            <img src={houseData.thumbnail_url}></img>
-            <div>
+            <img src={houseData.thumbnail_url} className="thumbnail"></img>
+            <div className="firstSection">
               <div className="location">
-                <h3>{houseData.street}</h3>
-                <p>{houseData.location}</p>
+                <h3 className="heading">{houseData.street}</h3>
+                <p className="secondaryText">{houseData.location}</p>
               </div>
-              <h3>{houseData.monthly_fee}/mesiac</h3>
+              <h3 className="heading">{houseData.monthly_fee}/mesiac</h3>
             </div>
             <div className="bedAndDiscount">
-              <div>
+              <div className="flexDiv">
                 <div className="bed">
-                  <BedOutlinedIcon></BedOutlinedIcon>
+                  <BedOutlinedIcon sx={{color:colorMain, marginRight: '0.8rem'}}></BedOutlinedIcon>
                   <p>{houseData.rooms}</p>
                 </div>
                 <div className="area">
-                  <StraightenOutlinedIcon></StraightenOutlinedIcon>
+                  <StraightenOutlinedIcon sx={{color:colorMain, marginRight: '0.8rem'}}></StraightenOutlinedIcon>
                   <p>{houseData.area}</p>
                 </div>
               </div>
               <p>{houseData.people_preference}</p>
             </div>
-            <p>Možnosti v okolí</p>
-            <div>
-              {houseData.facilities.map(d => (<p>{d}</p>))}
+            <p className="secondaryHeading Moznosti">Možnosti v okolí</p>
+            <div className="facilities">
+              {houseData.facilities.map((d, index) => (<span key={index} className="facility secondaryText">{d}</span>))}
             </div>
-            <p>Detailne info</p>
-            <p>{houseData.description}</p>
+            <p className="secondaryHeading">Detailne info</p>
+            <p className="secondaryText">{houseData.description}</p>
 
           </div>
           <div className="map">
-            <p>map</p>
+            <img src="/map.png" className="mappp"></img>
           </div>
         </div>
       </div>
