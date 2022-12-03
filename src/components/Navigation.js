b9661c5f6b20a434e4af90fa692e34b5697f26c3
Šimon Servatka <simon.servatka@student.tuke.sk>
2022-12-03 14:24:05 +0100

    react-roter

---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+
+
+function Navigation() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          LOGO
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <div className="nav-link active" aria-current="page">Home</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Navigation;
+  
