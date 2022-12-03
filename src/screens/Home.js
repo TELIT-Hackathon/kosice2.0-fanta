53c804fdc093af4ef3d112edf5a94dc7c6443212
Šimon Servatka <simon.servatka@student.tuke.sk>
2022-12-03 23:52:52 +0100

    home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,30 @@
-
+import { Link } from "react-router-dom";
+import "./Home.scss"
 
 function Home() {
   return (
     <div className="home">
-      <h1>Home</h1>
+
+      <div className="home-row">
+        <div className="home-col">
+
+          <h4>Pridanie nehnuteľnosti do prenájmu</h4>
+          <Link to="customer-form">
+            <button className="btn btn-primary">
+              Ďalej
+            </button>
+          </Link>
+        </div>
+        <div className="home-col">
+          <h4>Vyhladavanie nehnuteľnosti na prenájom</h4>
+          <Link to="results">
+            <button className="btn btn-primary">
+              Ďalej
+            </button>
+          </Link>
+        </div>
+      </div>
+
     </div>
   );
 }
