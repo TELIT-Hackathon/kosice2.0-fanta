098ed7894f632f0429fd5881bfc39505d7cc3758
Oleksandr Senych <oleksandr.senych@student.tuke.sk>
2022-12-04 08:16:37 +0100

    register

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,8 +1,9 @@
-import { auth } from '../firebase'
+﻿import { auth } from '../firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import React, { useEffect, useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { useNavigate } from "react-router-dom";
+import * as Yup from 'yup';
 
 
 const RegisterComponent = () => {
@@ -18,29 +19,39 @@ const RegisterComponent = () => {
             .catch(err => console.log(err,err.message))
     }
 
-
-
+    const schema = Yup.object().shape({
+        email: Yup.string()
+            .required('Povinné pole'),
+        password: Yup.string()
+            .required('Povinné pole')
+            .min(6, 'Heslo by malo obsahovať aspoň 6 znakov')
+    });
     return (
         <Formik
             initialValues={{
                 email: '',
                 password: ''
             }}
+            validationSchema={schema}
             onSubmit={(values) => { register(values) }}
         >
+        {({ errors, touched, isValidating }) => (
             <Form className='form' id='signupForm'>
                 <div className='form-container mb-4'>
                     <div className='form-body'>
-                        <div className='row '>
-                            <Field className="col form-control input-border" id='email-signup' name="email" type="email" placeholder="E-mail" />
-                            <Field className="col form-control input-border" id='password-signup' name="password" type="password" placeholder="Password" />
-                        </div>
+                            <div className='row '>
+                                <Field className={`form-control input-border ${errors.email ? "is-invalid" : ""}`} name="email" type="email" placeholder="E_mail" />
+                                <div className="invalid-feedback">{errors.email}</div>
+                                <div>&nbsp;</div>
+                               <Field className={`form-control input-border ${errors.password ? "is-invalid" : ""}`} name="password" type="password" placeholder="Password" />
+                                <div className="invalid-feedback">{errors.password}</div>
+                            </div>
                     </div>
                 </div>
                 <button type="submit" className="btn next btn-primary">Sign up</button>
-            </Form>
+                </Form>
+            )}
         </Formik>
     );
 }
-
 export default RegisterComponent;
